17ba406f6d60cd519f8f953cd6acca2c260b601e
bykov-mikhael <bykov.mikhael@yandex.ru>
2025-04-02 00:02:24 +0300

    внесены правки в задача про пароль

--- tsk-241.cpp
+++ tsk-241.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
 #include <cctype>
+#include <ios>
 #include <iostream>
-#include <source_location>
+#include <iterator>
+// #include <source_location>
 
 /**
  * Пароль от некоторого сервиса должен удовлетворять таким ограничениям:
@@ -15,56 +17,52 @@ constexpr int min_length{8};
 constexpr int max_length{14};
 constexpr int min_ch_code{33};
 constexpr int max_ch_code{126};
+constexpr int SYMBOLS_COUNT{3};
 
 struct answer {
-  bool password(std::string password) {
+  bool password(std::string& password) {
+    /*
+     * - состоять из символов таблицы ASCII с кодами от 33 до 126;
+     */
+    int numCode = std::ranges::count_if(
+        password.begin(), password.end(), [&numCode](auto ch) {
+          if ((static_cast<int>(ch) > min_ch_code) ||
+              (static_cast<int>(ch) < max_ch_code)) {
+            numCode++;
+          }
+          return numCode;
+        });
+
     /*
      * - быть не короче 8 символов и не длиннее 14;
      */
     if ((password.length() < min_length) || (password.length() > max_length)) {
       std::cout << "incorrect length password" << std::endl;
       return false;
+    } else {
+      std::cout << "correct length password" << std::endl;
     }
-
-    int numDigit =
-        std::ranges::count_if(password.begin(), password.end(), isdigit);
-
-    std::cout << "numDigit " << numDigit << std::endl;
-
-    if (numDigit > 0) {
-      return false;
-    }
-
+    /*
+     * - из 4 классов символов — большие буквы, маленькие буквы, цифры, прочие -
+     * символы — в пароле должны присутствовать не менее трёх любых.
+     */
     int numUpper =
         std::ranges::count_if(password.begin(), password.end(), isupper);
 
     std::cout << "numUpper " << numUpper << std::endl;
-
-    if (numUpper > 0) {
-      return false;
-    }
 
     int numLower =
         std::ranges::count_if(password.begin(), password.end(), islower);
 
     std::cout << "numLower " << numLower << std::endl;
 
-    if (numLower > 0) {
-      return false;
-    }
+    int numDigit =
+        std::ranges::count_if(password.begin(), password.end(), isdigit);
 
-    int numCode =
-        std::ranges::count_if(password.begin(), password.end(), [](auto ch) {
-          if ((static_cast<int>(ch) > min_ch_code) ||
-              (static_cast<int>(ch) < max_ch_code)) {
-            return true;
-          }
-          return false;
-        });
+    std::cout << "numDigit " << numDigit << std::endl;
 
-    std::cout << "numCode " << numCode << std::endl;
-
-    if (numCode > 0) {
+    if ((numUpper + numLower + numDigit) < SYMBOLS_COUNT) {
+      std::cout << "incorrect symbol count " << std::endl;
       return false;
     }
 
@@ -80,7 +78,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
 
   std::getline(std::cin, passwd);
 
-  std::cout << ans.password(passwd) << std::endl;
+  std::cout << std::boolalpha << ans.password(passwd) << std::endl;
 
   // std::source_location location = std::source_location::current();
 
